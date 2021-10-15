58c3ab54d75094b82cdf974c431ef790710b0c47
Mayank Patel <mayankkumar.patel1@nhs.net>
2021-10-15 16:08:44 +0100

    Add small toggles to change app color

--- lib/main.dart
+++ lib/main.dart
@@ -8,163 +8,174 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: HomePage()
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: HomePageElements(),
-      backgroundColor: Color.fromRGBO(0, 0, 0, 255),
-    );
-  }
+  HomePageElements createState() => HomePageElements();
 }
 
-class HomePageElements extends StatelessWidget {
+class HomePageElements extends State<HomePage> {
+  int _selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(top: 70.0, left: 30.0, right: 30.0, bottom: 20.0),
-      child: Column(
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Icon(FontAwesomeIcons.sortAmountDown, size: 28.0, color: Colors.white),
-              Icon(FontAwesomeIcons.userCircle, size: 30.0, color: Colors.white)
-            ],
-          ),
-          const SizedBox(height: 60.0),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              const Text('Welcome, Doctor Code Select An Option',
-                  textAlign: TextAlign.start,
-                  style: TextStyle(fontSize: 25.0, color: Colors.white)),
-              const SizedBox(height: 25.0),
-              const Text('Main Menu',
-                  textAlign: TextAlign.start,
-                  style: TextStyle(fontSize: 20.0, color: Colors.white)),
-            ],
-          ),
-          const SizedBox(height: 60.0),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return
+      Scaffold(
+        backgroundColor: _selectedIndex == 0 ? Color.fromRGBO(0, 0, 0, 255): Colors.white,
+        body: Container(
+          margin: EdgeInsets.only(top: 70.0, left: 30.0, right: 30.0, bottom: 20.0),
+          child: Column(
+            children: <Widget>[
+              GestureDetector(
+                onTap: () {
+                  setState(() => {
+                    if(_selectedIndex == 0) {
+                      _selectedIndex = 1
+                    } else {
+                      _selectedIndex = 0
+                    }
+                  });
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(FontAwesomeIcons.sortAmountDown, size: 28.0, color: _selectedIndex == 0 ? Colors.white : Colors.black),
+                    Icon(FontAwesomeIcons.userCircle, size: 30.0, color: _selectedIndex == 0 ? Colors.white : Colors.black),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 60.0),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Container(
-                          width: 150,
-                          height: 160,
-                          color: Color.fromRGBO(33,33,33,1),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: <Widget>[
-                              Icon(FontAwesomeIcons.clipboardList, size: 75.0,
-                              color: Color.fromRGBO(255,103,146, 1)),
-                              Column(
-                                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                                children: <Widget>[
-                                  Text("To-Do List", style: TextStyle(fontSize:20.0, color: Colors.white)),
-                                  Text("2 items", style: TextStyle(color: Colors.white))
-                                ],
-                              ),
-                            ],
-                          )
-                      ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                          width: 150,
-                          height: 160,
-                          color: Color.fromRGBO(33,33,33,1),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: <Widget>[
-                              Icon(FontAwesomeIcons.book, size: 75.0,
-                                  color: Color.fromRGBO(21,126,251,1)),
-                              Column(
-                                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                                children: <Widget>[
-                                  Text("Your notes", style: TextStyle(fontSize:20.0, color: Colors.white)),
-                                  Text("3 items", style: TextStyle(color: Colors.white))
-                                ],
-                              )
-                            ],
-                          ),
-                      ),
-                    ],
-                  ),
+                  const Text('Welcome, Doctor Code Select An Option',
+                      textAlign: TextAlign.start,
+                      style: TextStyle(fontSize: 25.0, color: Colors.white)),
+                  const SizedBox(height: 25.0),
+                  const Text('Main Menu',
+                      textAlign: TextAlign.start,
+                      style: TextStyle(fontSize: 20.0, color: Colors.white)),
                 ],
               ),
-              SizedBox(height: 50.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              const SizedBox(height: 60.0),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Container(
-                          width: 150,
-                          height: 160,
-                          color: Color.fromRGBO(33,33,33,1),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: <Widget>[
-                              Icon(FontAwesomeIcons.calendarCheck, size: 75.0,
-                                  color: Color.fromRGBO(108,99,255,1)),
-                              Column(
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Container(
+                              width: 150,
+                              height: 160,
+                              color: Color.fromRGBO(33,33,33,1),
+                              child: Column(
                                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                                 children: <Widget>[
-                                  Text("Agenda", style: TextStyle(fontSize:20.0, color: Colors.white)),
-                                  Text("8 items", style: TextStyle(color: Colors.white))
+                                  Icon(FontAwesomeIcons.clipboardList, size: 75.0,
+                                      color: Color.fromRGBO(255,103,146, 1)),
+                                  Column(
+                                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                    children: <Widget>[
+                                      Text("To-Do List", style: TextStyle(fontSize:20.0, color: Colors.white)),
+                                      Text("2 items", style: TextStyle(color: Colors.white))
+                                    ],
+                                  ),
                                 ],
                               )
-                            ],
                           ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            width: 150,
+                            height: 160,
+                            color: Color.fromRGBO(33,33,33,1),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: <Widget>[
+                                Icon(FontAwesomeIcons.book, size: 75.0,
+                                    color: Color.fromRGBO(21,126,251,1)),
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                  children: <Widget>[
+                                    Text("Your notes", style: TextStyle(fontSize:20.0, color: Colors.white)),
+                                    Text("3 items", style: TextStyle(color: Colors.white))
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
                     ],
                   ),
+                  SizedBox(height: 50.0),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Container(
-                          width: 150,
-                          height: 160,
-                          color: Color.fromRGBO(33,33,33,1),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: <Widget>[
-                              Icon(FontAwesomeIcons.cog, size: 75.0, color: Color.fromRGBO(255,182,108,1)),
-                              Column(
-                                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                                children: <Widget>[
-                                  Text("Settings", style: TextStyle(fontSize:20.0, color: Colors.white)),
-                                  Text("8 items", style: TextStyle(color: Colors.white))
-                                ],
-                              ),
-                            ],
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            width: 150,
+                            height: 160,
+                            color: Color.fromRGBO(33,33,33,1),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: <Widget>[
+                                Icon(FontAwesomeIcons.calendarCheck, size: 75.0,
+                                    color: Color.fromRGBO(108,99,255,1)),
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                  children: <Widget>[
+                                    Text("Agenda", style: TextStyle(fontSize:20.0, color: Colors.white)),
+                                    Text("8 items", style: TextStyle(color: Colors.white))
+                                  ],
+                                )
+                              ],
+                            ),
                           ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            width: 150,
+                            height: 160,
+                            color: Color.fromRGBO(33,33,33,1),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: <Widget>[
+                                Icon(FontAwesomeIcons.cog, size: 75.0, color: Color.fromRGBO(255,182,108,1)),
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                  children: <Widget>[
+                                    Text("Settings", style: TextStyle(fontSize:20.0, color: Colors.white)),
+                                    Text("8 items", style: TextStyle(color: Colors.white))
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
                     ],
                   ),
                 ],
-              ),
+              )
             ],
-          )
-        ],
-      ),
-    );
+          ),
+        ),
+      );
   }
 }
